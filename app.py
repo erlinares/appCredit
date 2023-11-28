433b5ed99b747a08aad87b10009a5f96e46f6c30
Edgar Rios Linares <erlinares@gmail.com>
2023-11-27 18:15:34 -0600

    Update app.py

--- app.py
+++ app.py
@@ -254,7 +254,7 @@ def modelling(dataset):
     models.append(('LDA', LinearDiscriminantAnalysis()))
     models.append(('KNN', KNeighborsClassifier()))
     models.append(('CART', DecisionTreeClassifier()))
-    #models.append(('NB', GaussianNB()))
+    models.append(('NB', GaussianNB()))
     #models.append(('RF', RandomForestClassifier()))
     #models.append(('SVM', SVC(gamma='auto')))
     #models.append(('XGBM', XGBClassifier()))
@@ -278,7 +278,7 @@ def modelling(dataset):
     resultsDF['Model'] = names
     #graficar resultados
     fig = go.Figure()
-    for i in range(4):
+    for i in range(5):
         fig.add_trace(go.Box(y=resultsBox[i:i+1].to_numpy()[0], name=names[i] ))
     st.plotly_chart(fig)
     return X_train, X_test, y_train, y_test
