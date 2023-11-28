d1a31e8862c1c6c29f10bb79411080c364d01677
Edgar Rios Linares <erlinares@gmail.com>
2023-11-27 18:08:40 -0600

    Update app.py

--- app.py
+++ app.py
@@ -283,7 +283,7 @@ def modelling(dataset):
         y=resultsBox[i:i+1].to_numpy()[0]
         st.write("names-", names[i])
         st.write("y-",y)
-    #st.plotly_chart(fig)
+    st.plotly_chart(fig)
     return X_train, X_test, y_train, y_test
 
 
