3be8b63a87a3e7d07aede81355a30944666a1634
Edgar Rios Linares <erlinares@gmail.com>
2023-11-27 18:04:26 -0600

    Update app.py

--- app.py
+++ app.py
@@ -280,7 +280,7 @@ def modelling(dataset):
     fig = go.Figure()
     for i in range(1):
         #fig.add_trace(go.Box(y=resultsBox[i:i+1].to_numpy()[0], name=names[i] ))
-        st,write("names-", names[i])
+        st.write("names-", names[i])
     #st.plotly_chart(fig)
     return X_train, X_test, y_train, y_test
 
